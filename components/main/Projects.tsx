3dd3651217dfd8af1a400dd733a19f1c5b77cedb
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-27 17:27:13 -0600

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -8,23 +8,35 @@ const Projects = () => {
       id="projects"
     >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        My Projects
+        Education
       </h1>
+      <div className="flex justify-center items-center gap-12 flex-wrap">
+        <ProjectCard
+          src="/uta.png"
+          title="University of Texas at Arlington"
+          description="Master's in Business Analyst (2023 - 2024)"
+        />
+        <ProjectCard
+          src="/sppu.jpg"
+          title="Pune University"
+          description="Bachelor's in Computer Science (2016 - 2020)"
+        />
+      </div>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/reality.png"
+          title="RealityAI"
+          description="Gen AI Data Analyst\n(January 2025-Present)"
         />
         <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/open.jpg"
+          title="OpenQQuantify"
+          description="Business Analyst (September 2024 - December 2024)"
         />
         <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/tripai.jpg"
+          title="TripAI"
+          description="Business Analyst (September 2024 - December 2024)"
         />
       </div>
     </div>
