b6e4b2405c3f9baea50078e29a4c737a2909b50d
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-27 17:27:20 -0600

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -11,6 +11,9 @@ import { SparklesIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
 const HeroContent = () => {
+  const handleHireMeClick = () => {
+    window.open("/Prasanna_Kottur_Business_Analyst..pdf", "_blank");
+  };
   return (
     <motion.div
       initial="hidden"
@@ -24,7 +27,7 @@ const HeroContent = () => {
         >
           <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
+            Business Analyst
           </h1>
         </motion.div>
 
@@ -33,12 +36,12 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
         >
           <span>
-            Providing
+            
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              Prasanna Kottur{" "}
             </span>
-            project exprience
+            Business Analyst
           </span>
         </motion.div>
 
@@ -46,14 +49,13 @@ const HeroContent = () => {
           variants={slideInFromLeft(0.8)}
           className="text-lg text-gray-400 my-5 max-w-[600px]"
         >
-          I&apos;m a Full Stack Software Engineer with experience in Website,
-          Mobile, and Software development. Check out my projects and skills.
+          I&apos;m a Business Analyst with experience in data analysis, process optimization, and requirement gathering. I specialize in turning complex business needs into actionable insights and solutions.
         </motion.p>
         <motion.a
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
         >
-          Learn More!
+          Resume
         </motion.a>
       </div>
 
