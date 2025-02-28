f684909d18ac252396423a05e0044a4c3a253446
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-27 21:15:30 -0600

    Update SkillText.tsx

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -26,7 +26,7 @@ const SkillText = () => {
         variants={slideInFromRight(0.5)}
         className='cursive text-[20px] text-gray-200 mb-10 mt-[10px] text-center'
         >
-            Never miss a task, deadline or idea
+            
         </motion.div>
     </div>
   )
