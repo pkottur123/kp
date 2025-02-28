4a75c856364e42f8ade65f38e50e7ac3eb4f3858
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-27 21:28:10 -0600

    Update Encryption.tsx

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -19,24 +19,7 @@ const Encryption = () => {
         </motion.div>
       </div>
 
-      <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
-        <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
-          <Image
-            src="/LockTop.png"
-            alt="Lock top"
-            width={50}
-            height={50}
-            className="w-[50px] translate-y-5 transition-all duration-200 group-hover:translate-y-11"
-          />
-          <Image
-            src="/LockMain.png"
-            alt="Lock Main"
-            width={70}
-            height={70}
-            className="z-10"
-          />
-        </div>
-      </div>
+      
 
       <div className="absolute z-[20] bottom-[10px] px-[5px]">
         <div className="cursive text-[20px] font-medium text-center text-gray-300"></div>
@@ -49,7 +32,7 @@ const Encryption = () => {
           autoPlay
           playsInline
           preload="false"
-          className="w-full h-auto opacity-10" // Hides the video if it's causing the blue tint
+          className="w-full h-auto opacity-15" // Hides the video if it's causing the blue tint
           src="/encryption.webm/"
         />
       </div> 
