3f0e4f587ae8a4f0939fd055466af3c8614afa08
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-27 20:18:42 -0600

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -7,21 +7,23 @@ const Projects = () => {
       className="flex flex-col items-center justify-center py-20"
       id="projects"
     >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+      <h1 className="text-[50px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         Education
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
           src="/uta.png"
           title="University of texas at Arlington"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          description="Master's in Business Analyst (2023 - 2024)"
         />
         <ProjectCard
-          src="/sppu.png"
+          src="/sppu.jpg"
           title="Pune University"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
+          description="Bachelor's in Computer Science (2016 - 2020)"
       </div>
+      <h1 className="text-[50px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-cyan-500 py-10">
+        Work Experience
+      </h1>
     </div>
   );
 };
