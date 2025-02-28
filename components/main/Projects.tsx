7918eb630a532df9fa47db2c4660833d93c09bf0
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-27 20:46:33 -0600

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -3,33 +3,45 @@ import ProjectCard from "../sub/ProjectCard";
 
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
-      <h1 className="text-[50px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+    <div className="flex flex-col items-center justify-center py-20">
+      {/* Section 1: Education */}
+      <h1 className="text-[50px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-cyan-500 py-10">
         Education
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+      <div className="flex justify-center items-center gap-12 flex-wrap">
         <ProjectCard
           src="/uta.png"
           title="University of Texas at Arlington"
-          description=""
+          description="Master's in Business Analyst (2023 - 2024)"
         />
         <ProjectCard
           src="/sppu.jpg"
           title="Pune University"
-          description=""
-        />
-        <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          description="Bachelor's in Computer Science (2016 - 2020)"
         />
       </div>
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        Experiance
+
+      {/* Section 2: Work Experience */}
+      <h1 className="text-[50px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-cyan-500 py-10">
+        Work Experience
       </h1>
+      <div className="flex justify-center items-center gap-12 flex-wrap">
+        <ProjectCard
+         src="/reality.png"
+         title="RealityAI"
+          description="Gen AI Data Analyst\n(January 2025-Present)"// ✅ New Line
+        />
+        <ProjectCard
+          src="/open.jpg"
+          title="OpenQQuantify"
+          description="Business Analyst (September 2024 - December 2024)"
+        />
+        <ProjectCard
+          src="/tripai.jpg"
+          title="TripAI"
+          description="Business Analyst (September 2024 - December 2024)"
+        />
+      </div>
     </div>
   );
 };
