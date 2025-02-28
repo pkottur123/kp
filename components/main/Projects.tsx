fbcf2b89cf0eea4b1ed829a14fa3da97349392a7
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-27 20:24:41 -0600

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
 
@@ -6,20 +7,29 @@ const Projects = () => {
     <div
       className="flex flex-col items-center justify-center py-20"
       id="projects"
-    >
-      <h1 className="text-[50px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+    > 
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        Work
+      </h1>
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         Education
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
           src="/uta.png"
-          title="University of texas at Arlington"
-          description="Master's in Business Analyst (2023 - 2024)"
+          title="University of Texas at Arlington"
+          description="Master's in Business Analyst (2023 - 2024)."
         />
         <ProjectCard
           src="/sppu.jpg"
-          title="Pune University"
-          description="Bachelor's in Computer Science (2016 - 2020)"
+          title="Bachelor's in Computer Science (2016 - 2020)"
+          description=""
+        />
+        <ProjectCard
+          src="/SpaceWebsite.png"
+          title="Space Themed Website"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
       </div>
     </div>
   );
