dc53e1319d9e6db2691d1650400036321e78b6c2
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-27 20:30:54 -0600

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -9,9 +9,6 @@ const Projects = () => {
       id="projects"
     > 
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
-        Work
-      </h1>
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         Education
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
@@ -25,10 +22,24 @@ const Projects = () => {
           title="Bachelor's in Computer Science (2016 - 2020)"
           description=""
         />
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        Experiance
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
         <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          src="/reality.png"
+          title="Reality AI"
+          description="Business Analyst (September 2024 - December 2024)"
+        />
+        <ProjectCard
+          src="/open.jpg"
+          title="OpenQQuantify"
+          description="Business Analyst (September 2024 - December 2024)"
+        />
+        <ProjectCard
+          src="/tripai.jpg"
+          title="TripAI"
+          description="Business Analyst (September 2024 - December 2024)"
         />
       </div>
     </div>
