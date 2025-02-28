d5cd89fbe39151eba616adda9bad9ab24f1cb384
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-27 21:18:35 -0600

    Update Encryption.tsx

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -8,9 +8,9 @@ import Image from "next/image";
 const Encryption = () => {
   return (
     <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full bg-black">
-      <div className="text-[60px] font-medium text-center text-white"></div> {/* Ensures full black background */}
+      <div className="absolute w-auto h-auto top-0 z-[5]"></div> {/* Ensures full black background */}
 
-      <div className="absolute w-auto h-auto top-0 z-[5]">
+      <div className="text-[60px] font-medium text-center text-white">
         <motion.div
           variants={slideInFromTop}
           className="text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-cyan-700"
