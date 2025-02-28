83910a7adedf39bd7b1a1baae0abe1bd8317ac67
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-27 20:41:00 -0600

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -30,7 +30,6 @@ const Projects = () => {
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
         Experiance
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
     </div>
   );
 };
