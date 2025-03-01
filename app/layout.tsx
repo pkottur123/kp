bf4f070c89d433811a2ca036c23c1e1f03e6dcd1
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-02-28 22:08:05 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
-import { Analytics } from "@vercel/analytics/react";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
