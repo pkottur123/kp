64b55f25274f46dd66ad19b90e4530c400a8f424
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-03-21 01:44:59 -0500

    Update ProjectCard.tsx

--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -12,7 +12,7 @@ const ProjectCard = ({ src, title, description, onButtonClick }: Props) => {
   return (
     <div className="w-[300px] flex flex-col items-center bg-[#1A1A2E] rounded-lg shadow-lg border border-[#2A0E61] p-4 transition-transform hover:scale-105 duration-300">
       <div className="relative w-full h-[120px] flex justify-center items-center">
-        <Image src={src} alt={title} width={200} height={100} className="rounded-t-lg" />
+        <Image src={src} alt={title} width={190} height={80} className="rounded-t-lg" />
       </div>
       <div className="w-full text-center mt-3">
         <h1 className="text-lg font-semibold text-white">{title}</h1>
