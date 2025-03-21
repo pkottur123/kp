e967c5c354941b6f5bf136265c21eca00d50ec7b
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-03-21 18:52:50 -0500

    Update Skills.tsx

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -13,90 +13,94 @@ import SkillText from "../sub/SkillText";
 
 const Skills = () => {
   return (
-    <section
-      id="skills"
-      className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
-      style={{ transform: "scale(0.9)" }}
-    >
-      <SkillText />
+    <>
+      <section
+        id="skills"
+        className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
+        style={{ transform: "scale(0.9)" }}
+      >
+        <SkillText />
 
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Skill_data.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Frontend_skill.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Backend_skill.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Full_stack.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-      <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
-        {Other_skill.map((image, index) => (
-          <SkillDataProvider
-            key={index}
-            src={image.Image}
-            width={image.width}
-            height={image.height}
-            index={index}
-          />
-        ))}
-      </div>
-
-      <div className="w-full h-full absolute">
-        <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
-          <video
-            className="w-full h-auto"
-            preload="false"
-            playsInline
-            loop
-            muted
-            autoPlay
-            src="/cards-video.webm"
-          />
+        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+          {Skill_data.map((image, index) => (
+            <SkillDataProvider
+              key={index}
+              src={image.Image}
+              width={image.width}
+              height={image.height}
+              index={index}
+            />
+          ))}
         </div>
-      </div>
-    </section>
 
-    <section id="additional-skills" className="flex flex-wrap justify-center gap-5 py-20">
-      {["Kotlin", "Rust", "Azure", "Redux", "Power BI", "FastAPI", "Terraform"].map((skill, index) => (
-        <div key={index} className="text-white text-xl font-bold">{skill}</div>
-      ))}
-    </section>
+        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+          {Frontend_skill.map((image, index) => (
+            <SkillDataProvider
+              key={index}
+              src={image.Image}
+              width={image.width}
+              height={image.height}
+              index={index}
+            />
+          ))}
+        </div>
+        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+          {Backend_skill.map((image, index) => (
+            <SkillDataProvider
+              key={index}
+              src={image.Image}
+              width={image.width}
+              height={image.height}
+              index={index}
+            />
+          ))}
+        </div>
+        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+          {Full_stack.map((image, index) => (
+            <SkillDataProvider
+              key={index}
+              src={image.Image}
+              width={image.width}
+              height={image.height}
+              index={index}
+            />
+          ))}
+        </div>
+        <div className="flex flex-row justify-around flex-wrap mt-4 gap-5 items-center">
+          {Other_skill.map((image, index) => (
+            <SkillDataProvider
+              key={index}
+              src={image.Image}
+              width={image.width}
+              height={image.height}
+              index={index}
+            />
+          ))}
+        </div>
+
+        <div className="w-full h-full absolute">
+          <div className="w-full h-full z-[-10] opacity-30 absolute flex items-center justify-center bg-cover">
+            <video
+              className="w-full h-auto"
+              preload="false"
+              playsInline
+              loop
+              muted
+              autoPlay
+              src="/cards-video.webm"
+            />
+          </div>
+        </div>
+      </section>
+
+      <section id="additional-skills" className="flex flex-wrap justify-center gap-5 py-20">
+        {[
+          "Kotlin", "Rust", "Azure", "Redux", "Power BI", "FastAPI", "Terraform"
+        ].map((skill, index) => (
+          <div key={index} className="text-white text-xl font-bold">{skill}</div>
+        ))}
+      </section>
+    </>
   );
 };
 
