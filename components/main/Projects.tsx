297943849252727b561c90f65c3ed6cf05788bea
pkottur123 <85752750+pkottur123@users.noreply.github.com>
2025-10-10 22:56:41 -0500

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -5,7 +5,35 @@ import ProjectCard from "../sub/ProjectCard";
 
 const Projects = () => {
   return (
-    <div className="flex flex-col items-center justify-center py-20" id="projects">
+     <div className="flex flex-col items-center justify-center py-20" id="projects">
+      {/* Section 1: Education */}
+      <h1 className="text-[50px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-cyan-500 py-10">
+        Education
+      </h1>
+      <div className="flex justify-center items-center gap-12 flex-wrap">
+        <ProjectCard
+          src="/uta.png"
+          title="University of Texas at Arlington"
+          description="Master&apos;s in Business Analytics (2023 - 2024)"
+        />
+        <ProjectCard
+          src="/sppu.jpg"
+          title="Savitribai Phule Pune University"
+          description="Bachelor&apos;s in Computer Science (2018 - 2022)"
+        />
+      </div>
+
+      {/* Section 2: Work Experience */}
+      <h1 className="text-[50px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-cyan-500 py-10">
+        Experience
+      </h1>
+      <div className="flex justify-center items-center gap-12 flex-wrap">
+        <ProjectCard src="/reality.png" title="RealityAI" description="Gen AI Data Analyst (January 2025 - Present)" />
+        <ProjectCard src="/open.jpg" title="OpenQQuantify" description="Business Analyst (September 2024 - December 2024)" />
+        <ProjectCard src="/tripai.jpg" title="TripAI" description="Business Analyst (September 2024 - December 2024)" />
+      </div>
+    
+     <div className="flex flex-col items-center justify-center py-20" id="projects">
       <h1 className="text-[50px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-cyan-500 py-10">
         Projects
       </h1>
